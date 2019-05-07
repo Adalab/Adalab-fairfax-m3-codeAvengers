8f68b9cfd34681c0670e36d27eb2572b9e23601d
superanika <anamaria.romerogarcia@gmail.com>
2019-05-07 11:07:04 +0200

    create Home component

--- src/App.js
+++ src/App.js
@@ -1,58 +1,10 @@
 import React from 'react';
 import './scss/main.scss';
-import logo from './images/awesome-logo.svg';
-import logoAdalab from './images/logo-adalab.png';
-import logoAvengers from './images/avengers.png';
+import Home from './components/Home';
 
 class App extends React.Component {
   render () {
-  return (
-    <div className="all">
-      <div className="landing__wrapper">
-        <div className="landing__subwrapper">
-          <header className="header__landing header__card">
-            <div className="header__logo-container"> <a href="index.html">
-            <h1 className="header__logo" style={{backgroundImage:`url(${logo})`}}>Title Awesome profile-cards</h1></a>
-            </div>
-          </header>
-          <main className="landing__main">
-            <div className="wrapper">
-              <h2 className="landing__main--title">Crea tu tarjeta de visita</h2>
-              <h3 className="landing__main--description">Crea mejores contactos profesionales de forma fácil y cómoda</h3>
-              <ul className="landing__list">
-                <li className="landing__list--item">
-                  <div className="landing__list--img"><i className="far fa-object-ungroup"></i>
-                  </div>
-                  <p className="landing__list--text">Diseña</p>
-                </li>
-                <li className="landing__list--item">
-                  <div className="landing__list--img"><i className="far fa-keyboard"></i>
-                  </div>
-                  <p className="landing__list--text">Rellena</p>
-                </li>
-                <li className="landing__list--item">
-                  <div className="landing__list--img"><i className="fas fa-share-alt"></i>
-                  </div>
-                  <p className="landing__list--text">Comparte</p>
-                </li>
-              </ul>
-              <div className="landing__main--link-container"> <a href="card.html" className="landing__main--link">Comenzar</a>
-              </div>
-            </div>
-          </main>
-        </div>
-      </div>
-        <footer className="footer-container">
-          <div className="footer-container__wraper">
-            <p className="footer__text">Awesome profile cards &copy2018</p> <a href="https://github.com/Adalab/fairfax-m2-catchFire"
-            target="_blank"><img src={logoAvengers} alt="logo grupo Catch Fire" className="footer__fire"/></a>
-            <a href="https://adalab.es/" className="footer__link" target="_blank">
-                <img src={logoAdalab} alt="logo adalab" className="footer__img"/>
-            </a>
-          </div>
-        </footer>
-    </div>
-  );
+  return <Home />
 }
 }
 
