b371e7a2a4ac01ff48acbc490d0e8dc5cfdac694
lararb <lararb91@gmail.com>
2019-05-11 12:03:06 +0200

    add icons links at preview.js

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -15,10 +15,18 @@ class Preview extends React.Component {
                         </div>
                         <div className="card__photo js__profile-image"></div>
                         <ul className="card__list">
-                            <li className="list__item list__item--email"></li>
-                            <li className="list__item list__item--phone"></li>
-                            <li className="list__item list__item--linkelind"></li>
-                            <li className="list__item list__item--github"></li>
+                            <li className="list__item list__item--email">
+                                <a href="mailto:${value}" class="card__list--link far fa-envelope email__icon" target="_blank">email</a>
+                            </li>
+                            <li className="list__item list__item--phone">
+                                <a href="tel:${value}" class="card__list--link fas fa-mobile-alt phone__icon" target="_blank">mobile</a>
+                            </li>
+                            <li className="list__item list__item--linkelind">
+                                <a href="https://www.linkedin.com/in/${value}" class="card__list--link fab fa-linkedin-in linkedin__icon" target="_blank">linkedin</a>
+                            </li>
+                            <li className="list__item list__item--github">
+                                <a href="https://github.com/${value}" class="card__list--link fab fa-github-alt github__icon" target="_blank">github</a>
+                            </li>
                         </ul>
                     </div>
                 </section>
