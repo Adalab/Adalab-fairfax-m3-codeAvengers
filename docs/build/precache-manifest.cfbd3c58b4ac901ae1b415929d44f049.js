57413abcacba94679757364f2c7664d6cb46c2d5
wczarnomysy <w.czarnomysy@gmail.com>
2019-05-28 23:52:47 +0200

    Create docs

---
+++ docs/build/precache-manifest.cfbd3c58b4ac901ae1b415929d44f049.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fef1ae59a380ccd4a52d6cf2612ec10c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "105d05088e36e682c081",
+    "url": "./static/css/main.2056fdb8.chunk.css"
+  },
+  {
+    "revision": "f36002f7a7f9b762fcfa",
+    "url": "./static/js/2.b0200f27.chunk.js"
+  },
+  {
+    "revision": "105d05088e36e682c081",
+    "url": "./static/js/main.f8bea4b9.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "8aacdf3e55fabf44242aed7a86b22496",
+    "url": "./static/media/avengers.8aacdf3e.png"
+  },
+  {
+    "revision": "0118e311c7fc2f113bee1abedecf92b2",
+    "url": "./static/media/awesome-logo.0118e311.svg"
+  }
+]);
